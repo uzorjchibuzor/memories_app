c97654abb97186d167880b2fb2522aa7fd636130
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-09-14 18:31:54 +0100

    journal can be created

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,37 @@
+class JournalsController < ApplicationController
+
+  def new
+    @journal = current_user.journals.build
+  end
+
+  def create
+    @journal = current_user.journals.create(journal_params)
+    if @journal.save
+      redirect_to @journal
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @journal = current_user.journals.find_by(id: params[:id])
+  end
+
+  def show
+    @journal = current_user.journals.find_by(id: params[:id])
+  end
+
+  def update
+    @journal = current_user.journals.find_by(id: params[:id])
+    redirect_to @journal if @journal.update_attribute(:title, params[:title])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def journal_params
+    params.require(:journal).permit(:title, :is_private)
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,13 @@
 FactoryBot.define do
+  factory :journal do
+    user
+    is_private { true }
+    title { Faker::Lorem.word }
+  end
+
+  factory :user do
+    name { Faker::Name.name }
+  end
+
   
 end
