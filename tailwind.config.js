d68d14ca308d8dd0242f4054c72c5091472a099a
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-09-18 15:50:48 +0100

    footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/views/**/*.html.haml',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        136: "400px",
+      },
+    },
+  },
+};
